24b377e4bc4a8dd167d7ed2a27a594105712d011
Shehroz Batt <101613341+ShehrozEdu@users.noreply.github.com>
2023-07-25 15:19:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,24 +16,28 @@ let weather = {
       })
       .then((data) => this.displayWeather(data));
   },
-  displayWeather: function (data) {
+displayWeather: function (data) {
     const { name } = data;
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
+    const link = `https://source.unsplash.com/1600x900/?${name}`;
+    document.body.style.background = `url('${link}')`;
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
+    document.querySelector(".weather").innerText = description;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
       "Wind speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-box").value);
+  },
+};
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
